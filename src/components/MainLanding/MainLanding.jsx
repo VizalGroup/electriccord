1f2186ef0ff16cf143f57b1cc96cba41215f48d2
Alejandrov98 <avillamayor720@gmail.com>
2024-04-09 07:31:31 -0300

    modificacion pedidas por el cliente

--- src/components/MainLanding/MainLanding.jsx
+++ src/components/MainLanding/MainLanding.jsx
@@ -8,36 +8,16 @@ export default function MainLanding() {
       <br />
       <br />
       <p className={Style.parr}>
-        En Electricord, somos tu destino confiable para todo lo relacionado con
-        el mantenimiento y la optimización de tu vehículo en la industria
-        automotriz. Con una pasión inquebrantable por la calidad y el
-        rendimiento, nos enorgullecemos de ofrecerte una amplia gama de
-        productos especializados, desde <strong>Fichas eléctricas </strong>
-        hasta <strong>Bobinas Bougicord</strong>,{" "}
-        <strong>Captores Bougicord</strong>, y <strong>Cables de Bujías</strong>
-        .<br />
-        <br />
-        ¿Qué nos distingue? Nuestra dedicación a la excelencia y nuestro
-        compromiso con la satisfacción del cliente. Ya sea que seas un
-        entusiasta del motor experimentado o simplemente estés buscando mantener
-        tu vehículo en óptimas condiciones, estamos aquí para proporcionarte las
-        soluciones que necesitas.
-        <br />
-        <br />
-        Explora nuestra selección cuidadosamente curada de productos de alta
-        calidad y descubre cómo podemos ayudarte a mejorar el rendimiento y la
-        fiabilidad de tu vehículo. Nuestro equipo experto está listo para
-        brindarte asesoramiento personalizado y responder a todas tus preguntas.
-        <br />
-        <br />
-        Para más información, <b><a href="/contacto" style={{color:'white',}}>contáctanos</a></b>
-       
-        .<br />
-        <br />
-        Únete a la comunidad de Electricord y experimenta la diferencia que
-        hacen nuestros productos en tu viaje automotriz. ¡Descubre la calidad,
-        la confiabilidad y el rendimiento que te ofrecemos en Electricord hoy
-        mismo!
+        Electricord ofrece todo para tu vehículo, desde{" "}
+        <strong>
+          fichas eléctricas, inyectores, cables de bujías, captores y bobinas
+          Bougicord.
+        </strong>
+        {" "}Asesoramiento, explora nuestra selección de productos de alta calidad Si
+        tienes Taller de reparación o local de venta de repuestos, solicita una
+        cotización a través de WhatsApp o contáctanos al{" "}
+        <a href="https://wa.me/3516658905"> 3516658905</a> o{" "}
+        <a href="https://wa.me/3516845357">3516845357</a>
       </p>
       <br />
     </div>
